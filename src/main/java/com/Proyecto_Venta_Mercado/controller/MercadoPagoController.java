9fb4c1c4b874259b2ce3daa2f3462d83056adb6e
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-06-23 11:00:43 -0500

    Permitir CORS para frontend de producción

--- src/main/java/com/Proyecto_Venta_Mercado/controller/MercadoPagoController.java
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/MercadoPagoController.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/mercadopago")
 public class MercadoPagoController {
 
