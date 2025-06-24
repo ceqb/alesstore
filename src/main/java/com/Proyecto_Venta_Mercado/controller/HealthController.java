1b06d73648efb19a4161b8cf101783fa04b6c971
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-06-24 11:43:15 -0500

    Fix CORS allowedOrigins vs allowCredentials

--- src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://alesstore-prueba.netlify.app", "https://alesstore.onrender.com"})
+
 @RequestMapping("/api/v1")
 public class HealthController {
     @GetMapping("/health")
--- src/main/java/com/Proyecto_Venta_Mercado/controller/MercadoPagoController.java
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/MercadoPagoController.java
@@ -21,7 +21,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = {"*"})
 @RequestMapping("/mercadopago")
 public class MercadoPagoController {
 
