c01757ede082ad3a2170d3f13e218a407263008f
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-06-24 09:28:35 -0500

    Agregar endpoint /health para monitoreo

---
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.Proyecto_Venta_Mercado.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
