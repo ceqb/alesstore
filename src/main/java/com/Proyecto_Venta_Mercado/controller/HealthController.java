75b16e2e5c888fff77b4989a7fb9ad242260be43
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-06-24 09:42:58 -0500

    Fix endpoint /api/v1/health

--- src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
@@ -2,10 +2,11 @@ package com.Proyecto_Venta_Mercado.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api/v1")
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
