d1650dd4fb3e0027487e0679868277ca482c454c
Carlos Eugenio Quiroz Balarezo <cquiroz@imarpe.gob.pe>
2025-06-25 08:43:19 -0500

    Forzar deploy para crear tabla Prueba

--- src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
+++ src/main/java/com/Proyecto_Venta_Mercado/controller/HealthController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
-@RequestMapping("/api/v1")
 public class HealthController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
